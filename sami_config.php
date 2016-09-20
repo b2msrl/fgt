3bff97f5bcfbb5f181c40ec4c7321d0bc81011bb
alevento <a@a.it>
2016-09-20 17:00:06 +0200

    Substitute

--- sami_config.php
+++ sami_config.php
@@ -19,10 +19,10 @@ $versions = GitVersionCollection::create($dir)
 
 return new Sami($iterator,array(
     'theme'                => 'default',
-    'title'                => '@@@package_name API',
+    'title'                => 'fgt API',
     'versions'             => $versions,
-    'build_dir'            => '@@@doc_destination/build/%version%',
-    'cache_dir'            => '@@@doc_destination/cache/%version%',
+    'build_dir'            => 'Y:/Public/laravel-packages/www/doc/b2msrl/fgt/build/%version%',
+    'cache_dir'            => 'Y:/Public/laravel-packages/www/doc/b2msrl/fgt/cache/%version%',
     'default_opened_level' => 2,
 ));
 
